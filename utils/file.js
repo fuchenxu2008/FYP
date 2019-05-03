6eede6896fe3b4f903e05d42e177220bffd82a86
fuchenxu2008 <fuchenxu2008@163.com>
2019-05-03 17:12:11 +0800

    remove event-stream

--- utils/file.js
+++ utils/file.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const es = require('event-stream');
 const path = require('path');
 
 exports.saveJSON = async (filePath, json) => new Promise((resolve, reject) => {
@@ -24,34 +23,3 @@ exports.readJSON = async (filePath) => new Promise((resolve, reject) => {
         reject(error);
     }
 });
-
-exports.readTxt = (filePath) => new Promise((resolve, reject) => {
-    try {
-        fs.createReadStream(path.resolve(process.cwd(), filePath))
-            .pipe(es.split())
-            .pipe(es.mapSync(line => {
-                this.parseLine(line);
-            }))
-            .on('error', (err) => reject(err))
-            .on('end', () => resolve())
-    } catch (error) {
-        reject(error);
-    }
-})
-
-exports.parseLine = (line) => {
-    let regex = /([-|\d|.]+)|(<.*>)/g
-    const result = line.match(regex);
-    if (result.length !== 6) {
-        console.log('result: ', result);
-        throw new Error('Line parsed error');
-    }
-    return {
-        edge_id: result[0],
-        start_node_id: result[1],
-        end_node_id: result[2],
-        length_of_the_edge: result[3], // in meters
-        speed_limit: result[4], // in meters per second
-        geometry: result[5],
-    }
-}
